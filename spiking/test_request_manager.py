c01fdd8e5552d8e94fe8bfedc7f4cf9ce65682a7
Apoorv Kansal <akan57@student.monash.edu>
2017-07-11 00:20:19 +1000

    Testing code to analyse the request manager root class

--- spiking/test_request_manager.py
+++ spiking/test_request_manager.py
@@ -1,18 +1,43 @@
-import unittest, mock
+from unittest import mock
+import unittest
 
-from ticket_requester import ZRequestManager
+import requests
 
+from ticket_requester import ZCoreRequestManager
+
+
+@mock.patch('ticket_requester.requests.get')
 class RequestManagerTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.request_mgr = ZRequestManager(subdomain="homesrus", username="apoorv.k.kansal@gmail.com", password="ZenDeskChallenge15!9793")
+        self.request_mgr = ZCoreRequestManager(subdomain="homesrus-aus", username="apoorv.k.kansal@gmail.com",
+                                               password="ZenDeskChallenge15!9793")
 
-
-    @mock.patch('ticket_requester.requests.get')
     def test_get_success(self, mock_get_req):
+
         mock_get_req.return_value = mock.Mock()
         mock_get_req.return_value.status_code = 200
+        mock_get_req.return_value.headers = {'content-type': 'application/json'}
 
         response = self.request_mgr._get("tickets.json")
 
         self.assertEqual(response.status_code, 200)
+
+    def test_get_not_found(self, mock_get_req):
+
+        mock_get_req.return_value = mock.Mock(status=404)
+        mock_get_req.return_value.raise_for_status.side_effect = requests.exceptions.HTTPError("Not found.")
+
+        self.assertRaises(requests.exceptions.HTTPError, self.request_mgr._get, "non-existent-resource.json")
+
+    def test_get_received_plain_txt(self, mock_get_req):
+        mock_get_req.return_value = mock.Mock()
+        mock_get_req.return_value.raise_for_status.side_effect = requests.exceptions.HTTPError("Not found.")
+
+        self.assertRaises(requests.exceptions.HTTPError, self.request_mgr._get, "non-existent-resource.json")
+
+
+
+
+
+
