68d63e90349d53f72804af0e0083e47867e74bb0
Apoorv Kansal <akan57@student.monash.edu>
2017-07-14 17:56:13 +1000

    Setup angular project and imported css template

---
+++ ZTicketViewerFrontEnd/ticket_viewer_client_app/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { SIDEBAR_TOGGLE_DIRECTIVES } from './shared/sidebar.directive';
+import { AsideToggleDirective } from './shared/aside.directive';
+
+// Routing Module
+import { AppRoutingModule } from './app.routing';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
+    ChartsModule
+  ],
+  declarations: [
+    AppComponent,
+    SIDEBAR_TOGGLE_DIRECTIVES,
+    AsideToggleDirective
+  ],
+  providers: [{
+    provide: LocationStrategy,
+    useClass: HashLocationStrategy
+  }],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
